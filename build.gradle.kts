09222cedaa6a9057258ae262983e4ae6a12c8a20
Daniel Slapman <danslapman@gmail.com>
2019-11-21 14:17:06 +0300

    Crossbuilding with gradle-crossbuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,25 @@
+@file:Suppress("DEPRECATION")
+
 buildscript {
     dependencies {
-        classpath("com.adtran:scala-multiversion-plugin:1.+")
+        classpath("gradle.plugin.com.github.maiflai:gradle-scalatest:0.25")
     }
 }
 
 plugins {
-    scala
+    id("com.github.prokod.gradle-crossbuild").version("0.9.1")
 }
 
 allprojects {
-    apply(plugin = "scala")
-    apply(plugin = "com.adtran.scala-multiversion-plugin")
-
-    val scalaCompilerPlugin by configurations.creating
-
     repositories {
         mavenCentral()
         jcenter()
     }
 
-    dependencies {
-        implementation("org.scala-lang:scala-library:%scala-version%")
-        testImplementation("org.scalatest:scalatest_%%:3.0.8")
-        testImplementation("junit:junit:4.12")
-        scalaCompilerPlugin("com.olegpy:better-monadic-for_%%:0.3.1")
+    configurations {
+        create("scalaCompilerPlugin") {
+            setTransitive(false)
+        }
     }
 }
 
@@ -31,11 +27,40 @@ subprojects {
     group = "danslapman"
     version = "0.1"
 
-    tasks.withType<ScalaCompile>().configureEach {
-        val plugins = configurations["scalaCompilerPlugin"]!!.files().map { "-Xplugin:${it.absolutePath}" }
+    apply(plugin = "scala")
+    apply(plugin = "com.github.prokod.gradle-crossbuild")
+    apply(plugin = "com.github.maiflai.scalatest")
 
-        scalaCompileOptions.additionalParameters = plugins +
-            listOf("-feature", "-Xfatal-warnings")
+    crossBuild {
+        scalaVersionsCatalog = mapOf(
+                "2.12" to "2.12.10",
+                "2.13" to "2.13.0"
+        )
+
+        builds {
+            create("v212")
+        }
+    }
+
+    dependencies {
+        compile("org.scala-lang:scala-library:2.12.10")
+        testCompile("org.scalatest:scalatest_2.12:3.0.8")
+        "scalaCompilerPlugin" ("com.olegpy:better-monadic-for_2.12:0.3.1")
+        testRuntime("org.pegdown:pegdown:1.4.2")
+    }
+
+    val copyPlugins by tasks.registering(Copy::class) {
+        from(configurations["scalaCompilerPlugin"])
+        into("$buildDir/scalac-plugins")
+    }
+
+    tasks.withType<ScalaCompile>().configureEach {
+        val plugins = File("$buildDir/scalac-plugins").listFiles()?.let {
+            "-Xplugin:" + it.joinToString(",")
+        }
+
+        scalaCompileOptions.additionalParameters =
+            listOf("-feature", "-Xfatal-warnings") + listOfNotNull(plugins)
     }
 }
 
